5e1d58de8f9dd79232ad28cc7de257383bee1967
moritz theiselmann <hello@moritztheiselmann.de>
2022-12-06 12:49:39 +0100

    get media device by streamid and disdplay on page

--- src/content-script.ts
+++ src/content-script.ts
@@ -4,7 +4,6 @@ import { Message, MessageListener } from './types';
 
 const requests = new Map<Messages, MessageListener>();
 
-let streamID = <string>'';
 const dialogSuccess = (sender: chrome.runtime.MessageSender, data: any, sendResponse: any)  => {
   if (!sender) {
     throw new Error('Received message contains no sender');
@@ -14,12 +13,40 @@ const dialogSuccess = (sender: chrome.runtime.MessageSender, data: any, sendResp
     throw new Error('Received message contains no data.');
   }
   
-  streamID = data.streamID;
+  startScreenSteramFrom(data.streamID);
 
   sendResponse(`received streamid: ${data.streamID}`);
 
   return true;
 }
+
+const startScreenSteramFrom = async (streamID : string) => {
+  const constraints = <MediaStreamConstraints>{
+    audio: false,
+    video: {
+      mandatory: {
+        chromeMediaSource: 'desktop',
+        chromeMediaSourceId: streamID
+      }
+    }
+  };
+
+  console.log(constraints);
+
+  try {
+    const stream = await (navigator.mediaDevices as any).getUserMedia(constraints);
+    overlay.srcObject = stream; 
+    overlay.play();
+  }
+  catch(err) {
+    console.error(err);
+  }
+
+  // navigator.mediaDevices.getUserMedia(constraints)
+  // .then(handleStreamSuccess)
+  // .catch(handleStreamError);
+}
+
 
 const dialogCancel = (sender: chrome.runtime.MessageSender, data: any, sendResponse: any)  => {
   sendResponse(`received message: ${data.data}`);
@@ -47,6 +74,37 @@ const listenForMessages = () => {
     return request(sender, data, sendResponse);
   });
 }
+
+const overlayWrapper = document.createElement('div');
+overlayWrapper.className = 'video-capture-stream__overlay-wrapper';
+
+const overlay = document.createElement('video');
+overlay.className = 'video-capture-stream__overlay';
+overlayWrapper.appendChild(overlay);
+
+overlayWrapper.style.cssText = `
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  z-index: 99999;
+
+  display: flex;
+  justify-content: center;
+  
+  align-items: center;
+  pointer-events:none;
+  touch-action: none;
+  background-color: blue !important;
+`;
+
+overlay.style.cssText = `
+  position: relative;
+  width: 500px;
+  height: 500px;
+  background-color: blue !important;
+`;
+
+document.body.appendChild(overlay);
 
 const init = ():void => {
   registerMessengerRequests();
